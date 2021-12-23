53c59f9197cc5ca4d143d1e77f81107df0db6859
Michael Griebling <sam@c-inspirations.com>
2021-12-23 09:31:15 -0500

    Test change.

--- Tests/DecimalsTests/DecimalsTests.swift
+++ Tests/DecimalsTests/DecimalsTests.swift
@@ -4,13 +4,6 @@ import XCTest
 final class DecimalsTests: XCTestCase {
 
     func testMiscellaneous() {
-        let number: HDecimal = "12345678901234567890.12345678901234567890"
-        let number2 = HDecimal(5432109)
-        print("\(number)+\(number2) =", number+number2)
-        print("π =", HDecimal.pi)
-        HDecimal.digits = 128
-        print("2¹⁵⁶ =", HDecimal(2).pow(HDecimal(156)))
-        
         // Test Decimal to int conversions        
         let n = -1234567890123456789
         XCTAssertEqual(n, Decimal128(n).int)
